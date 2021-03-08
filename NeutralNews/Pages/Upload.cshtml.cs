3f01d2c594e3b22e5f8e45658fe31fe6882b05a5
audreyboyd <60332315+audreyboyd@users.noreply.github.com>
2021-03-08 12:59:29 -0500

    Upload function, works and can add the link to the log.txt file.

--- NeutralNews/Pages/Upload.cshtml.cs
+++ NeutralNews/Pages/Upload.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -15,5 +16,18 @@ namespace NeutralNews.Pages
         {
             Message = "Insert a valid link to an article/website you would like to know the political leaning of.";
         }
+
+            [BindProperty]
+            public string inputURL { get; set; }
+
+            public void OnPost()
+            {
+                using (StreamWriter writerURL = new StreamWriter("log.txt"))
+                {
+                    writerURL.WriteLine(inputURL);
+                }
+                Message = "You submitted a link! Please wait while we check the political leaning of it's site.";
+
+            }
     }
 }
