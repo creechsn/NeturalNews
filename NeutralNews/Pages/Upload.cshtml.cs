c85f134c1b25725350ab68e054a2c9851682b6ab
audreyboyd <60332315+audreyboyd@users.noreply.github.com>
2021-03-19 14:57:32 -0400

    Dont pull this commit, code not fully working yet!

--- NeutralNews/Pages/Upload.cshtml.cs
+++ NeutralNews/Pages/Upload.cshtml.cs
@@ -1,33 +1,74 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace NeutralNews.Pages
 {
     public class UploadModel : PageModel
     {
         public string Message { get; set; }
-
+       
         public void OnGet()
         {
             Message = "Insert a valid link to an article/website you would like to know the political leaning of.";
         }
 
-            [BindProperty]
-            public string inputURL { get; set; }
+        [BindProperty]
+        public string inputURL { get; set; }
 
-            public void OnPost()
+        public void OnPost()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection myConnection = new SqlConnection(conString);
+            myConnection.Open();
+            //todo get the connection to work properly
+
+            //trims down the url so it can be compared to our db
+            Uri urlTrim = new Uri(inputURL);
+            string host = urlTrim.Host;
+            bool flag = false;
+
+            
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from [Reference_Data]";
+            cmd.Connection = myConnection;
+
+            //reads through the db and looks for a match to the user's input
+            SqlDataReader read = cmd.ExecuteReader();
+            while (read.Read())
             {
+                if (read[1].ToString() == host)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag == true)
+            {
+                SqlCommand cmd2 = new SqlCommand("select ReferenceScore from Reference_Data where ([ReferenceURL] = @hostURL)", myConnection);
+                cmd2.Parameters.AddWithValue("@hostURL", host);
+
+                //to-do show the user the political leaning
+            }
+            else
+            {
+                //Appends the url to a log file, so that our team can manually add it
+                //todo appears to write over the first slot in the log file
                 using (StreamWriter writerURL = new StreamWriter("log.txt"))
                 {
-                    writerURL.WriteLine(inputURL);
+                    writerURL.WriteLine(host);
                 }
-                Message = "You submitted a link! Please wait while we check the political leaning of it's site.";
-
+                Message = "Uh oh! Looks like we do not know the political leaning for the website you entered.";
             }
+            myConnection.Close();
+        }
     }
 }
